678e9f51cf01faa4983bfb773a4ab1915afe7470
Jordan <mason.jordanp@gmail.com>
2016-12-05 18:44:48 -0600

    Added button disable

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
-var app = angular.module('myApp', ['levelUp']);
+angular.module('myApp', [])
   .controller('appCtrl', function($scope) {
     $scope.score = 0;
     $scope.total = 0;
     $scope.answer = false;
     $scope.display = "You've answered" + $scope.score + "/" + $scope.total + "correctly!";
   })
-  .factory('levelUp', function($scope.answer, $scope.score, $scope.total) {
-      var addToScore = function() {
-        if ($scope.answer === true) {
-          $scope.score++;
-          $scope.total++;
-          return $scope.display;
-        } else {
-          $scope.total++;
-          return $scope.display;
-        }
-    };
+  .controller('buttonCtrl', function($scope) {
+    $scope.isDisabled = false;
+    $scope.choose = function() {
+      alert('This works!');
+      $scope.isDisabled = true;
+    }
+  })
+  // .factory('levelUp', function($scope.answer, $scope.score, $scope.total) {
+  //     var addToScore = function() {
+  //       if ($scope.answer === true) {
+  //         $scope.score++;
+  //         $scope.total++;
+  //         return $scope.display;
+  //       } else {
+  //         $scope.total++;
+  //         return $scope.display;
+  //       }
+  //   };
 
-    return {
-      levelUp: levelUp
-    };
-  });
+  //   return {
+  //     levelUp: levelUp
+  //   };
+  // });
