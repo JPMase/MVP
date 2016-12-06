2e43a2c7d19913039b27477ac55be955964accac
Jordan <mason.jordanp@gmail.com>
2016-12-05 18:05:11 -0600

    Added basic JS

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['levelUp']);
+  .controller('appCtrl', function($scope) {
+    $scope.score = 0;
+    $scope.total = 0;
+    $scope.answer = false;
+    $scope.display = "You've answered" + $scope.score + "/" + $scope.total + "correctly!";
+  })
+  .factory('levelUp', function($scope.answer, $scope.score, $scope.total) {
+      var addToScore = function() {
+        if ($scope.answer === true) {
+          $scope.score++;
+          $scope.total++;
+          return $scope.display;
+        } else {
+          $scope.total++;
+          return $scope.display;
+        }
+    };
+
+    return {
+      levelUp: levelUp
+    };
+  });
